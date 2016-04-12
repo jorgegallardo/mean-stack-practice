9b3b7a4f65550d209327ef8d4393933129f7e059
Jorge Gallardo <Jorge Gallardo>
2016-04-11 21:54:13 -0400

    added api route on server

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
+app.get('/quotes', function(req, res) {
+  var quotes = ['The way you do anything, should be the way you do everything.', 'It wasn\'t me.'];
+  res.send(quotes);
+});
+
 app.listen(3000, function() {
   console.log('Listening on port 3000.');
 });
