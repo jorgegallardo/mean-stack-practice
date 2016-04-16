3c7b45494ce993d6de091a83a59d671c652c0e81
Jorge Gallardo <Jorge Gallardo>
2016-04-16 19:59:50 -0400

    connect to mongo database

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
 
-var quotes = ['The way you do anything, should be the way you do everything.',
-              'It wasn\'t me.',
-              'Just do it.'];
+// Connection URL 
+var url = 'mongodb://localhost:27017/quotes';
+// Use connect method to connect to the Server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to database server.");
+  db.close();
+});
+
+// var quotes = ['The way you do anything, should be the way you do everything.',
+//               'It wasn\'t me.',
+//               'Just do it.'];
 
 // parse application/json 
 app.use(bodyParser.json());
