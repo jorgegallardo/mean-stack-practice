d1c4c161275815dab65339f5c136f37acbcd038a
Jorge Gallardo <Jorge Gallardo>
2016-04-17 14:17:16 -0400

    connect to local mongodb server

--- server.js
+++ server.js
@@ -2,35 +2,54 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
-
-// Connection URL 
+var assert = require('assert'); // module used for writing unit tests for applications
 var url = 'mongodb://localhost:27017/quotes';
+
 // Use connect method to connect to the Server
 MongoClient.connect(url, function(err, db) {
+  //ensure that we've connected
   assert.equal(null, err);
-  console.log("Connected correctly to database server.");
+  console.log("Successfully connected to database server.");
   db.close();
 });
-
-// var quotes = ['The way you do anything, should be the way you do everything.',
-//               'It wasn\'t me.',
-//               'Just do it.'];
 
 // parse application/json 
 app.use(bodyParser.json());
 
 app.use(express.static('public'));
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
+// app.get('/', function(req, res) {
+//   res.send('Hello World!');
+// });
 
 app.get('/quotes', function(req, res) {
-  res.send(quotes);
+  var quotesArray = [];
+  MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    var cursor = db.collection('quotes');
+    cursor.find().forEach(function(doc, err) {
+      assert.equal(null, err);
+      quotesArray.push(doc);
+    }, function() {
+      res.send(quotesArray);
+      db.close();
+    });
+  });
 });
 
 app.post('/quotes', function(req, res) {
+  // var item = {
+  //   text: req.body.newQuote
+  // };
+  // MongoClient.connect(url, function(err, db) {
+  //   assert.equal(null, err);
+  //   db.collection('quotes').insertOne(item, function(err, result) {
+  //     assert.equal(null, err);
+  //     console.log('Quote inserted.');
+  //     db.close();
+  //   });
+  // });
+  // res.redirect('/');
   quotes.push(req.body.newQuote);
   res.send(); //server will hang if this isn't sent
 });
